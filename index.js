b6e3d396ab8acbf39983520aa003ba420cf0b9f9
Carlos Amado <exchangeside@gmail.com>
2023-04-28 04:00:49 -0400

    fixing cors error

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { locationsArray } from './locationObjects.js'
 const PORT = process.env.PORT || 8005;
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
