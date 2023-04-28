afc35e87220ee36c71780bb3ea8ccfea178a3b1a
Carlos Amado <exchangeside@gmail.com>
2023-04-28 03:10:33 -0400

    created new API service

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ import { locations } from './locationObjects.js'
 import { locationsArray } from './locationObjects.js'
 const PORT = process.env.PORT || 8005;
 import express from 'express'
-// import cors from 'cors'
-// import axios from "axios"
 import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express();
-// app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
